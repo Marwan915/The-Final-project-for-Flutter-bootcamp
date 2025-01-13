a21255d15d73385c67b3e926d558d7b0ad26a44b
marwan ali <MarwanBinAbdullah0@gmail.com>
2025-01-13 12:16:21 +0300

    but api key of weather in .env file

--- lib/service/api.dart
+++ lib/service/api.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
   // جلب درجة الحرارة من API
   Future<double> fetchTemperature({required String city}) async {
+    final String? apid = dotenv.env['API_KEY'];
     final String url =
-        'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=aee9c7791155d732b86f7407193f57d8&units=metric';
+        'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apid&units=metric';
 
     try {
       final response = await http.get(Uri.parse(url));
